098a469618e8846c1b4c8db7ec84f9466393bedd
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-04-27 20:14:29 +0100

    - Added config file.

---
+++ config/eloquent-filtering.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'suppress' => [
+        'filter' => [
+            'denied'           => false,
+            'missing'          => false,
+            'invalid'          => false,
+            'malformed_format' => false,
+        ],
+        'sort' => [
+            'denied' => false,
+        ],
+    ],
+
+    'custom_filters' => [
+
+    ],
+];
