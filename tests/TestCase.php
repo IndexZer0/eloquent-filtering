bf95cc1b5e963768ca0bc4c00275236a279b4e30
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-04-27 20:24:20 +0100

    - Setup database for tests.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -15,6 +15,8 @@ class TestCase extends Orchestra
         Factory::guessFactoryNamesUsing(
             fn (string $modelName) => 'IndexZer0\\EloquentFiltering\\Database\\Factories\\'.class_basename($modelName).'Factory'
         );
+
+        $this->setUpDatabase($this->app);
     }
 
     protected function getPackageProviders($app)
@@ -24,13 +26,68 @@ class TestCase extends Orchestra
         ];
     }
 
-    public function getEnvironmentSetUp($app)
+    public function getEnvironmentSetUp($app): void
     {
         config()->set('database.default', 'testing');
+        config()->set('app.env', 'local');
+        config()->set('database.default', 'sqlite');
+        config()->set('database.connections.sqlite', [
+            'driver'   => 'sqlite',
+            'database' => ':memory:',
+            'prefix'   => '',
+        ]);
 
         /*
         $migration = include __DIR__.'/../database/migrations/create_eloquent-filtering_table.php.stub';
         $migration->up();
         */
+    }
+
+    protected function setUpDatabase($app): void
+    {
+        $schema = $app['db']->connection()->getSchemaBuilder();
+
+        $schema->create('authors', function (Blueprint $table): void {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->timestamps();
+        });
+
+        $schema->create('author_profiles', function (Blueprint $table): void {
+            $table->id();
+            $table->foreignIdFor(Author::class);
+            $table->tinyInteger('age')->nullable();
+            $table->timestamps();
+        });
+
+        $schema->create('books', function (Blueprint $table): void {
+            $table->id();
+            $table->foreignIdFor(Author::class);
+            $table->string('title');
+            $table->string('description');
+            $table->timestamps();
+        });
+
+        $schema->create('comments', function (Blueprint $table): void {
+            $table->id();
+            $table->foreignIdFor(Book::class);
+            $table->string('content');
+            $table->timestamps();
+        });
+
+        $schema->create('products', function (Blueprint $table): void {
+            $table->id();
+            $table->foreignIdFor(Manufacturer::class);
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->timestamps();
+        });
+
+        $schema->create('manufacturers', function (Blueprint $table): void {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+
     }
 }
