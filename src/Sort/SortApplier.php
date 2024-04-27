86d4ead2898a2bf63c74471f4271fcc9a5710f5f
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-04-27 20:23:10 +0100

    - Added SortApplier.

---
+++ src/Sort/SortApplier.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace IndexZer0\EloquentFiltering\Sort;
+
+use Illuminate\Database\Eloquent\Builder;
+use IndexZer0\EloquentFiltering\Suppression\Suppression;
+use IndexZer0\EloquentFiltering\Sort\Contracts\SortableList;
+
+readonly class SortApplier
+{
+    public function __construct(public SortableList $sortableList)
+    {
+    }
+
+    public function apply(Builder $query, array $sorts): Builder
+    {
+        foreach ($sorts as $sort) {
+            Suppression::honour(
+                fn () => $this->applySort($query, $sort),
+            );
+        }
+
+        return $query;
+    }
+
+    private function applySort(Builder $query, array $sort): Builder
+    {
+        $this->sortableList->ensureAllowed($sort['target']);
+
+        return $query->orderBy($sort['target'], $sort['value']);
+    }
+}
