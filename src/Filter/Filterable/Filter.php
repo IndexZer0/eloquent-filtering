76b21956baa88b0260a020e150614e37b31e1b6c
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-04-30 18:22:23 +0100

    - Update Filter static api.

--- src/Filter/Filterable/Filter.php
+++ src/Filter/Filterable/Filter.php
@@ -4,27 +4,46 @@ declare(strict_types=1);
 
 namespace IndexZer0\EloquentFiltering\Filter\Filterable;
 
-use IndexZer0\EloquentFiltering\Filter\Contracts\FilterableDefinition;
+use IndexZer0\EloquentFiltering\Filter\AllowedFilters\AllowedColumn;
+use IndexZer0\EloquentFiltering\Filter\AllowedFilters\AllowedCustomFilter;
+use IndexZer0\EloquentFiltering\Filter\AllowedFilters\AllowedJsonColumn;
+use IndexZer0\EloquentFiltering\Filter\AllowedFilters\AllowedRelation;
+use IndexZer0\EloquentFiltering\Filter\Contracts\AllowedFilter;
+use IndexZer0\EloquentFiltering\Filter\Contracts\FilterableList;
 
 class Filter
 {
-    public static function all(): UnrestrictedFilterableList
+    public static function allowAll(): UnrestrictedFilterableList
     {
         return new UnrestrictedFilterableList();
     }
 
-    public static function allow(FilterableDefinition ...$allowedDefinitions): RestrictedFilterableList
+    public static function allowOnly(AllowedFilter ...$allowedFilters): RestrictedFilterableList
     {
-        return new RestrictedFilterableList(...$allowedDefinitions);
+        return new RestrictedFilterableList(...$allowedFilters);
     }
 
-    public static function column(string $target, array $types): FilterableColumn
+    public static function column(string $target, array $types, ?string $alias = null): AllowedColumn
     {
-        return new FilterableColumn($target, $types);
+        return new AllowedColumn($target, $types, $alias);
     }
 
-    public static function relation(string $target, array $types, FilterableDefinition ...$filterableDefinitions): FilterableRelation
+    public static function jsonColumn(string $target, array $types): AllowedColumn
     {
-        return new FilterableRelation($target, $types, $filterableDefinitions);
+        return new AllowedJsonColumn($target, $types);
+    }
+
+    public static function relation(
+        string $target,
+        array $types,
+        FilterableList $allowedFilters = new RestrictedFilterableList(),
+        ?string $alias = null
+    ): AllowedRelation {
+        return new AllowedRelation($target, $types, $allowedFilters, $alias);
+    }
+
+    public static function custom(array $types): AllowedFilter
+    {
+        return new AllowedCustomFilter($types);
     }
 }
