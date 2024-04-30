920e4c6b8b508621d3bc7fa1f0735563ccd4ca70
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-04-30 22:52:58 +0100

    - Added more OrFilter tests.

--- tests/Filter/FilterMethods/ConditionFilters/OrFilterTest.php
+++ tests/Filter/FilterMethods/ConditionFilters/OrFilterTest.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+use IndexZer0\EloquentFiltering\Filter\Exceptions\DeniedFilterException;
+use IndexZer0\EloquentFiltering\Filter\Exceptions\MalformedFilterFormatException;
 use IndexZer0\EloquentFiltering\Filter\Filterable\Filter;
 use IndexZer0\EloquentFiltering\Tests\TestingResources\Models\Author;
 
@@ -80,3 +82,117 @@ it('can perform $or filter | multiple exists', function (): void {
     expect($models->count())->toBe(2);
 
 });
+
+it('has the correct DeniedFilterException message', function (): void {
+    Author::filter(
+        [
+            [
+                'type' => '$or',
+                'value' => [
+                    [
+                        'target' => 'name',
+                        'type'   => '$eq',
+                        'value'  => 'George Raymond Richard Martin',
+                    ],
+                    [
+                        'target' => 'name',
+                        'type'   => '$eq',
+                        'value'  => 'J. R. R. Tolkien',
+                    ]
+                ]
+            ],
+        ],
+        Filter::allowNone()
+    );
+
+})->throws(DeniedFilterException::class, "\"\$or\" filter is not allowed");
+
+it('must have at least two child filters', function (
+    array $value_container,
+    ?string $expected_sql,
+    bool    $expect_exception
+): void {
+
+    if ($expect_exception) {
+        $this->expectException(MalformedFilterFormatException::class);
+        $this->expectExceptionMessage('"$or" filter does not match required format.');
+    }
+
+    $query = Author::filter(
+        [
+            [
+                'type' => '$or',
+                ...$value_container
+            ],
+        ],
+        Filter::allowAll()
+    );
+
+    expect($query->toRawSql())->toBe($expected_sql);
+
+})->with([
+    'no value' => [
+        'value_container'  => [],
+        'expected_sql'     => null,
+        'expect_exception' => true,
+    ],
+    'value not array' => [
+        'value_container'  => ['value' => true],
+        'expected_sql'     => null,
+        'expect_exception' => true,
+    ],
+    'value empty array' => [
+        'value_container'  => ['value' => []],
+        'expected_sql'     => null,
+        'expect_exception' => true,
+    ],
+    'value only one element' => [
+        'value_container'  => ['value' => [
+            [
+                'target' => 'name',
+                'type'   => '$eq',
+                'value'  => 'George Raymond Richard Martin',
+            ]
+        ]],
+        'expected_sql'     => null,
+        'expect_exception' => true,
+    ],
+    'value two elements' => [
+        'value_container'  => ['value' => [
+            [
+                'target' => 'name',
+                'type'   => '$eq',
+                'value'  => 'George Raymond Richard Martin',
+            ],
+            [
+                'target' => 'name',
+                'type'   => '$eq',
+                'value'  => 'J. R. R. Tolkien',
+            ]
+        ]],
+        'expected_sql'     => 'select * from "authors" where (("name" = \'George Raymond Richard Martin\') or ("name" = \'J. R. R. Tolkien\'))',
+        'expect_exception' => false,
+    ],
+    'value three elements' => [
+        'value_container'  => ['value' => [
+            [
+                'target' => 'name',
+                'type'   => '$eq',
+                'value'  => 'George Raymond Richard Martin',
+            ],
+            [
+                'target' => 'name',
+                'type'   => '$eq',
+                'value'  => 'J. R. R. Tolkien',
+            ],
+            [
+                'target' => 'name',
+                'type'   => '$eq',
+                'value'  => 'J. K. Rowling',
+            ]
+        ]],
+        'expected_sql'     => 'select * from "authors" where (("name" = \'George Raymond Richard Martin\') or ("name" = \'J. R. R. Tolkien\') or ("name" = \'J. K. Rowling\'))',
+        'expect_exception' => false,
+    ],
+
+]);
