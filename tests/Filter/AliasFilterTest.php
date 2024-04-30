9d507bbeb45181e38b2a796e06814d9a6218d6cc
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-04-30 18:28:38 +0100

    - Added AliasFilterTest.

---
+++ tests/Filter/AliasFilterTest.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+use IndexZer0\EloquentFiltering\Filter\Contracts\FilterableList;
+use IndexZer0\EloquentFiltering\Filter\Filterable\Filter;
+use IndexZer0\EloquentFiltering\Tests\TestingResources\Models\Author;
+
+it('uses alias when specified', function (
+    array          $filter,
+    FilterableList $allowed_filters,
+    string         $expected_sql
+): void {
+
+    $query = Author::filter(
+        [
+            $filter,
+        ],
+        $allowed_filters
+    );
+
+    expect($query->toRawSql())->toBe($expected_sql);
+
+})->with([
+    'column filter' => [
+        'filter' => [
+            'type'   => '$eq',
+            'target' => 'name',
+            'value'  => 'George Raymond Richard Martin',
+        ],
+        'allowed_filters' => Filter::allowOnly(
+            Filter::column('name', ['$eq'], 'name_alias')
+        ),
+        'expected_sql' => 'select * from "authors" where "name_alias" = \'George Raymond Richard Martin\'',
+    ],
+    'relation filter' => [
+        'filter' => [
+            'type'   => '$has',
+            'target' => 'documents',
+            'value'  => [],
+        ],
+        'allowed_filters' => Filter::allowOnly(
+            Filter::relation('documents', ['$has'], alias: 'books')
+        ),
+        'expected_sql' => 'select * from "authors" where exists (select * from "books" where "authors"."id" = "books"."author_id")',
+    ],
+]);
