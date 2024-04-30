dc0e3d7bca398053ad31d40db664a1dcbe3d6918
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-04-30 18:20:50 +0100

    - Updated FilterableList.

--- src/Filter/Contracts/FilterableList.php
+++ src/Filter/Contracts/FilterableList.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace IndexZer0\EloquentFiltering\Filter\Contracts;
 
+use IndexZer0\EloquentFiltering\Filter\Filterable\PendingFilter;
+
 interface FilterableList
 {
-    public function ensureAllowed(string $type, ?string $target): FilterableList;
+    public function ensureAllowed(PendingFilter $pendingFilter): PendingFilter;
 }
