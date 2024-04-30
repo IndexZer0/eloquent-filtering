94ee2b8935303be1de52dcba42b751b6f07f4924
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-04-30 18:27:52 +0100

    - Added LatestFilter test custom filter.

---
+++ tests/TestingResources/CustomFilters/LatestFilter.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace IndexZer0\EloquentFiltering\Tests\TestingResources\CustomFilters;
+
+use Illuminate\Database\Eloquent\Builder;
+use IndexZer0\EloquentFiltering\Filter\FilterMethods\Abstract\AbstractCustomFilter;
+
+class LatestFilter extends AbstractCustomFilter
+{
+    /*
+     * -----------------------------
+     * Interface methods
+     * -----------------------------
+     */
+
+    public static function type(): string
+    {
+        return '$latest';
+    }
+
+    public static function format(): array
+    {
+        return [];
+    }
+
+    public function apply(Builder $query): Builder
+    {
+        return $query->latest();
+    }
+}
