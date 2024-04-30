9967b27488b7e1b03e864974063f2ea6ef0b632a
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-04-30 18:21:25 +0100

    - Updated DeniedFilterException to use PendingFilter.

--- src/Filter/Exceptions/DeniedFilterException.php
+++ src/Filter/Exceptions/DeniedFilterException.php
@@ -7,12 +7,13 @@ namespace IndexZer0\EloquentFiltering\Filter\Exceptions;
 use Exception;
 use IndexZer0\EloquentFiltering\Contracts\SuppressibleException;
 use IndexZer0\EloquentFiltering\Filter\Contracts\FilterException;
+use IndexZer0\EloquentFiltering\Filter\Filterable\PendingFilter;
 
 class DeniedFilterException extends Exception implements FilterException, SuppressibleException
 {
-    public function __construct(string $type, ?string $target)
+    public function __construct(PendingFilter $pendingFilter)
     {
-        parent::__construct("\"{$type}\" filter for \"{$target}\" is not allowed");
+        parent::__construct($pendingFilter->getDeniedMessage());
     }
 
     public function shouldSuppress(): bool
