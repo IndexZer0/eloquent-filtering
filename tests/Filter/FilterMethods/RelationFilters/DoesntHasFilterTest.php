d784c5f8c0dcc65df12fa4aaf0f205417be23525
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-04-30 18:30:16 +0100

    - Created RelationFilters tests.

---
+++ tests/Filter/FilterMethods/RelationFilters/DoesntHasFilterTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+use IndexZer0\EloquentFiltering\Filter\Filterable\Filter;
+use IndexZer0\EloquentFiltering\Tests\TestingResources\Models\Author;
+
+beforeEach(function (): void {
+    $this->createAuthors();
+});
+
+it('can perform $doesntHas filter', function (): void {
+    $query = Author::filter(
+        [
+            [
+                'target' => 'books',
+                'type'   => '$doesntHas',
+                'value'  => [],
+            ],
+        ],
+        Filter::allowOnly(
+            Filter::relation('books', ['$doesntHas']),
+        )
+    );
+
+    $expectedSql = <<< SQL
+        select * from "authors" where not exists (select * from "books" where "authors"."id" = "books"."author_id")
+        SQL;
+
+    expect($query->toRawSql())->toBe($expectedSql);
+
+    $models = $query->get();
+
+    expect($models->count())->toBe(0);
+
+});
---
+++ tests/Filter/FilterMethods/RelationFilters/HasFilterTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+use IndexZer0\EloquentFiltering\Filter\Filterable\Filter;
+use IndexZer0\EloquentFiltering\Tests\TestingResources\Models\Author;
+
+beforeEach(function (): void {
+    $this->createAuthors();
+});
+
+it('can perform $has filter', function (): void {
+    $query = Author::filter(
+        [
+            [
+                'target' => 'books',
+                'type'   => '$has',
+                'value'  => [],
+            ],
+        ],
+        Filter::allowOnly(
+            Filter::relation('books', ['$has']),
+        )
+    );
+
+    $expectedSql = <<< SQL
+        select * from "authors" where exists (select * from "books" where "authors"."id" = "books"."author_id")
+        SQL;
+
+    expect($query->toRawSql())->toBe($expectedSql);
+
+    $models = $query->get();
+
+    expect($models->count())->toBe(2);
+
+});
