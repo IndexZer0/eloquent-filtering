869cc5b456f1b46967bb4801f22e78d223cf2813
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-05-01 23:30:11 +0100

    - Split Alias test to multiple tests so that Target::alias has code coverage.

--- tests/Filter/AliasFilterTest.php
+++ tests/Filter/AliasFilterTest.php
@@ -2,47 +2,52 @@
 
 declare(strict_types=1);
 
-use IndexZer0\EloquentFiltering\Filter\Contracts\FilterableList;
 use IndexZer0\EloquentFiltering\Filter\Filterable\Filter;
 use IndexZer0\EloquentFiltering\Filter\Target\Target;
 use IndexZer0\EloquentFiltering\Tests\TestingResources\Models\Author;
 
-it('uses alias when specified', function (
-    array          $filter,
-    FilterableList $allowed_filters,
-    string         $expected_sql
-): void {
+it('can alias column | column filter', function (): void {
 
     $query = Author::filter(
         [
-            $filter,
+            [
+                'type'   => '$eq',
+                'target' => 'name',
+                'value'  => 'George Raymond Richard Martin',
+            ],
         ],
-        $allowed_filters
+        Filter::only(
+            Filter::column(Target::alias('name', 'name_alias'), ['$eq'])
+        )
     );
 
-    expect($query->toRawSql())->toBe($expected_sql);
+    $expectedSql = <<< SQL
+        select * from "authors" where "name_alias" = 'George Raymond Richard Martin'
+        SQL;
 
-})->with([
-    'column filter' => [
-        'filter' => [
-            'type'   => '$eq',
-            'target' => 'name',
-            'value'  => 'George Raymond Richard Martin',
+    expect($query->toRawSql())->toBe($expectedSql);
+
+});
+
+it('can alias relation | relation filter', function (): void {
+
+    $query = Author::filter(
+        [
+            [
+                'type'   => '$has',
+                'target' => 'documents',
+                'value'  => [],
+            ],
         ],
-        'allowed_filters' => Filter::only(
-            Filter::column(Target::alias('name', 'name_alias'), ['$eq'])
-        ),
-        'expected_sql' => 'select * from "authors" where "name_alias" = \'George Raymond Richard Martin\'',
-    ],
-    'relation filter' => [
-        'filter' => [
-            'type'   => '$has',
-            'target' => 'documents',
-            'value'  => [],
-        ],
-        'allowed_filters' => Filter::only(
+        Filter::only(
             Filter::relation(Target::alias('documents', 'books'), ['$has'])
-        ),
-        'expected_sql' => 'select * from "authors" where exists (select * from "books" where "authors"."id" = "books"."author_id")',
-    ],
-]);
+        )
+    );
+
+    $expectedSql = <<< SQL
+        select * from "authors" where exists (select * from "books" where "authors"."id" = "books"."author_id")
+        SQL;
+
+    expect($query->toRawSql())->toBe($expectedSql);
+
+});
