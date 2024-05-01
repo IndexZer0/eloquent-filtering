ed5b9c03c17f9fa2f1c4799b775235305ff7e3ca
IndexZer0 <IndexZer0@users.noreply.github.com>
2024-05-01 19:24:32 +0000

    Fix styling

--- src/Filter/Target/Alias.php
+++ src/Filter/Target/Alias.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace IndexZer0\EloquentFiltering\Filter\Target;
 
 readonly class Alias
@@ -7,5 +9,6 @@ readonly class Alias
     public function __construct(
         public string  $target,
         public ?string $targetAlias = null
-    ) {}
+    ) {
+    }
 }
