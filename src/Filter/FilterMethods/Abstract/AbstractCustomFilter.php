d88e2d767d0bf19ff9e021b782ad4e4d70d1da26
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-05-04 20:13:45 +0100

    - Moved some methods to AbstractCustomFilter.

--- src/Filter/FilterMethods/Abstract/AbstractCustomFilter.php
+++ src/Filter/FilterMethods/Abstract/AbstractCustomFilter.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace IndexZer0\EloquentFiltering\Filter\FilterMethods\Abstract;
 
 use IndexZer0\EloquentFiltering\Filter\Contracts\FilterMethod;
+use IndexZer0\EloquentFiltering\Filter\Filterable\ApprovedFilter;
 
 abstract class AbstractCustomFilter implements FilterMethod
 {
@@ -17,5 +18,15 @@ abstract class AbstractCustomFilter implements FilterMethod
     public static function usage(): string
     {
         return FilterMethod::USAGE_CUSTOM;
+    }
+
+    public static function format(): array
+    {
+        return [];
+    }
+
+    public static function from(ApprovedFilter $approvedFilter): static
+    {
+        return new static();
     }
 }
