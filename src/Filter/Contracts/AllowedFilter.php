e6da741c8802d1687a0f66c96c4018a51449bed7
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-05-04 20:07:36 +0100

    - Update AllowedFilter interface.

--- src/Filter/Contracts/AllowedFilter.php
+++ src/Filter/Contracts/AllowedFilter.php
@@ -8,9 +8,9 @@ use IndexZer0\EloquentFiltering\Filter\Filterable\PendingFilter;
 
 interface AllowedFilter
 {
-    public function allowedFilters(): FilterableList;
+    public function allowedFilters(): AllowedFilterList;
 
     public function matches(PendingFilter $pendingFilter): bool;
 
-    public function hydrate(PendingFilter $pendingFilter): PendingFilter;
+    public function getTarget(PendingFilter $pendingFilter): ?Target;
 }
