eba450094807eec951461fef12edf50251274a5e
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-05-04 20:13:16 +0100

    - Update Filterable.php

--- src/Filter/Traits/Filterable.php
+++ src/Filter/Traits/Filterable.php
@@ -7,7 +7,7 @@ namespace IndexZer0\EloquentFiltering\Filter\Traits;
 use Illuminate\Database\Eloquent\Builder;
 use IndexZer0\EloquentFiltering\Filter\Contracts\FilterApplier;
 use IndexZer0\EloquentFiltering\Filter\Contracts\FilterParser;
-use IndexZer0\EloquentFiltering\Filter\Contracts\FilterableList;
+use IndexZer0\EloquentFiltering\Filter\Contracts\AllowedFilterList;
 use IndexZer0\EloquentFiltering\Filter\Filterable\Filter;
 
 trait Filterable
@@ -15,12 +15,12 @@ trait Filterable
     public function scopeFilter(
         Builder $query,
         array $filters,
-        ?FilterableList $filterableList = null
+        ?AllowedFilterList $allowedFilterList = null
     ): Builder {
 
         /** @var FilterParser $filterParser */
         $filterParser = resolve(FilterParser::class);
-        $filters = $filterParser->parse($filters, $filterableList ?? $this->allowedFilters());
+        $filters = $filterParser->parse($filters, $allowedFilterList ?? $this->allowedFilters());
 
         /** @var FilterApplier $filterApplier */
         $filterApplier = resolve(FilterApplier::class);
@@ -30,7 +30,7 @@ trait Filterable
         );
     }
 
-    protected function allowedFilters(): FilterableList
+    protected function allowedFilters(): AllowedFilterList
     {
         $defaultAllowedList = config('eloquent-filtering.default_allowed_list', 'none');
 
