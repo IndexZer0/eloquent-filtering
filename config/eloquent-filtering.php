8b19eb804dd99d739a96c2ec897e336a227b3191
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-05-04 22:12:49 +0100

    - pint fixes.

--- config/eloquent-filtering.php
+++ config/eloquent-filtering.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 return [
     'default_allowed_filter_list' => 'none',
-    'default_allowed_sort_list' => 'none',
+    'default_allowed_sort_list'   => 'none',
 
     'suppress' => [
         'filter' => [
