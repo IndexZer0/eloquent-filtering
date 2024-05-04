6db96430e5b2737dba8d9a49f04764e97984a9d6
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-05-04 22:10:06 +0100

    - Renamed SortableList -> AllowedSortList.

--- src/Sort/Contracts/AllowedSortList.php
+++ src/Sort/Contracts/AllowedSortList.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace IndexZer0\EloquentFiltering\Sort\Contracts;
 
-interface SortableList
+interface AllowedSortList
 {
-    public function ensureAllowed(string $column): bool;
+    public function ensureAllowed(string $field): bool;
 }
