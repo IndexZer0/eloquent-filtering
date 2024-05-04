325e188ab2e6995f139baf68084f739145156c3c
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-05-04 20:16:59 +0100

    - Added DuplicateFilter::from().

--- tests/TestingResources/CustomFilters/DuplicateFilter.php
+++ tests/TestingResources/CustomFilters/DuplicateFilter.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace IndexZer0\EloquentFiltering\Tests\TestingResources\CustomFilters;
 
 use Illuminate\Database\Eloquent\Builder;
+use IndexZer0\EloquentFiltering\Filter\Filterable\ApprovedFilter;
 use IndexZer0\EloquentFiltering\Filter\FilterMethods\Abstract\AbstractColumnFilter;
 
 class DuplicateFilter extends AbstractColumnFilter
@@ -28,5 +29,10 @@ class DuplicateFilter extends AbstractColumnFilter
     public function apply(Builder $query): Builder
     {
         return $query->where('column', 'value');
+    }
+
+    public static function from(ApprovedFilter $approvedFilter): static
+    {
+        return new static();
     }
 }
