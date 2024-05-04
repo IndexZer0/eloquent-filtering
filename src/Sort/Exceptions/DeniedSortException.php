687093488d089b5dd912460465ca07c62928e098
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-05-04 22:10:21 +0100

    - Rename $column to $field.

--- src/Sort/Exceptions/DeniedSortException.php
+++ src/Sort/Exceptions/DeniedSortException.php
@@ -10,9 +10,9 @@ use IndexZer0\EloquentFiltering\Sort\Contracts\SortException;
 
 class DeniedSortException extends Exception implements SortException, SuppressibleException
 {
-    public static function throw(string $column): void
+    public static function throw(string $field): void
     {
-        throw new self("\"{$column}\" sort is not allowed");
+        throw new self("\"{$field}\" sort is not allowed");
     }
 
     public function shouldSuppress(): bool
