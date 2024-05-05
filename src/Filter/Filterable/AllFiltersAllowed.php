5df1440c21e92e91440c9f4d48ccdd216a47cf66
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-05-05 01:38:15 +0100

    - Use Target::alias in AllFiltersAllowed.

--- src/Filter/Filterable/AllFiltersAllowed.php
+++ src/Filter/Filterable/AllFiltersAllowed.php
@@ -7,7 +7,7 @@ namespace IndexZer0\EloquentFiltering\Filter\Filterable;
 use IndexZer0\EloquentFiltering\Filter\Contracts\AllowedFilterList;
 use IndexZer0\EloquentFiltering\Filter\Contracts\FilterMethod;
 use IndexZer0\EloquentFiltering\Filter\Traits\EnsuresChildFiltersAllowed;
-use IndexZer0\EloquentFiltering\Target\AliasedTarget;
+use IndexZer0\EloquentFiltering\Target\Target;
 
 class AllFiltersAllowed implements AllowedFilterList
 {
@@ -18,7 +18,7 @@ class AllFiltersAllowed implements AllowedFilterList
         $childFilters = $this->ensureChildFiltersAllowed($pendingFilter, $this);
 
         return $pendingFilter->approveWith(
-            $pendingFilter->is(FilterMethod::USAGE_CONDITION) ? null : new AliasedTarget($pendingFilter->desiredTarget()),
+            $pendingFilter->is(FilterMethod::USAGE_CONDITION) ? null : Target::alias($pendingFilter->desiredTarget()),
             $childFilters
         );
     }
