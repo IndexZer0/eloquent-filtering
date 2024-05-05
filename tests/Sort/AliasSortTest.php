da14d685467e1880d866b295d20492a2b4386870
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-05-05 01:39:23 +0100

    - Added AliasSortTest.

---
+++ tests/Sort/AliasSortTest.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+use IndexZer0\EloquentFiltering\Sort\Sortable\Sort;
+use IndexZer0\EloquentFiltering\Target\Target;
+use IndexZer0\EloquentFiltering\Tests\TestingResources\Models\Author;
+use IndexZer0\EloquentFiltering\Tests\TestingResources\Models\AuthorProfile;
+
+beforeEach(function (): void {
+    Author::create([
+        'id'   => 1,
+        'name' => 'Name 1',
+    ]);
+    AuthorProfile::create([
+        'age'       => 20,
+        'author_id' => 1,
+    ]);
+
+    Author::create([
+        'id'   => 2,
+        'name' => 'Name 2',
+    ]);
+    AuthorProfile::create([
+        'age'       => 30,
+        'author_id' => 2,
+    ]);
+
+    Author::create([
+        'id'   => 3,
+        'name' => 'Name 3',
+    ]);
+    AuthorProfile::create([
+        'age'       => 40,
+        'author_id' => 3,
+    ]);
+});
+
+it('can alias field | field sort', function (): void {
+
+    $query = Author::sort(
+        [
+            [
+                'target' => 'name',
+                'value'  => 'desc',
+            ],
+        ],
+        Sort::only(
+            Sort::field(Target::alias('name', 'name_alias'))
+        )
+    );
+
+    $expectedSql = <<< SQL
+        select * from "authors" order by "name_alias" desc
+        SQL;
+
+    expect($query->toRawSql())->toBe($expectedSql);
+
+});
