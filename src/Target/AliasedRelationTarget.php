eefeb088fb98ecd7b2a484b27d92bfab3f0f1819
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-05-06 20:31:17 +0100

    - AliasedRelationTarget extend AliasedTarget.

--- src/Target/AliasedRelationTarget.php
+++ src/Target/AliasedRelationTarget.php
@@ -6,7 +6,7 @@ namespace IndexZer0\EloquentFiltering\Target;
 
 use IndexZer0\EloquentFiltering\Contracts\Target as TargetContract;
 
-readonly class AliasedRelationTarget implements TargetContract
+readonly class AliasedRelationTarget extends AliasedTarget
 {
     public array $targets;
 
@@ -20,24 +20,9 @@ readonly class AliasedRelationTarget implements TargetContract
 
     /*
      * -----------------------------
-     * Interface methods
+     * AliasedRelationTarget methods
      * -----------------------------
      */
-
-    public function isFor(string $target): bool
-    {
-        return $this->target === $target;
-    }
-
-    public function getReal(): string
-    {
-        return $this->targetAlias ?? $this->target;
-    }
-
-    public function target(): string
-    {
-        return $this->target;
-    }
 
     public function getChildrenTargets(): array
     {
