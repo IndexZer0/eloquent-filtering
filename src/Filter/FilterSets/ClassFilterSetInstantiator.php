6c95bc1196f8cf347eb75c7eca607b9212d4ef0f
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-05-13 00:37:16 +0100

    - Added ClassFilterSetInstantiator class.

---
+++ src/Filter/FilterSets/ClassFilterSetInstantiator.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace IndexZer0\EloquentFiltering\Filter\FilterSets;
+
+use IndexZer0\EloquentFiltering\Filter\Contracts\FilterSet as FilterSetContract;
+
+class ClassFilterSetInstantiator
+{
+    public function __invoke(string|FilterSetContract $filterSet): FilterSetContract
+    {
+        if (!is_string($filterSet)) {
+            return $filterSet;
+        }
+
+        if (!is_a($filterSet, FilterSetContract::class, true)) {
+            throw new \Exception('TODO'); // TODO
+        }
+
+        return new $filterSet;
+    }
+}
