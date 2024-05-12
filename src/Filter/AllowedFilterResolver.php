e0cef455c710dca13282be79ecd869de9f9ca91a
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-05-13 00:39:55 +0100

    - Update AllowedFilterResolver to get filters from filter sets.

--- src/Filter/AllowedFilterResolver.php
+++ src/Filter/AllowedFilterResolver.php
@@ -4,19 +4,69 @@ declare(strict_types=1);
 
 namespace IndexZer0\EloquentFiltering\Filter;
 
+use IndexZer0\EloquentFiltering\Contracts\IsFilterable;
 use IndexZer0\EloquentFiltering\Filter\Contracts\AllowedFilterList;
+use IndexZer0\EloquentFiltering\Filter\Contracts\FilterSet;
+use IndexZer0\EloquentFiltering\FilterSetAllowedFilterResolver;
 
 class AllowedFilterResolver
 {
     public function __construct(
-        protected AllowedFilterList $allowedFilterList,
-        protected string $modelFqcn
+        protected FilterSet|AllowedFilterList|string|null $allowedFilters,
+        protected IsFilterable $model
     ) {
 
     }
 
     public function resolve(): AllowedFilterList
     {
-        return $this->allowedFilterList->resolveRelationsAllowedFilters($this->modelFqcn);
+        $allowedFilterList = $this->resolveToAllowedFilterList();
+
+        return $allowedFilterList->resolveRelationsAllowedFilters($this->model::class);
+    }
+
+    private function resolveToAllowedFilterList(): AllowedFilterList
+    {
+        if ($this->allowedFilters instanceof AllowedFilterList) {
+            return $this->allowedFilters;
+        }
+
+        if ($this->allowedFilters === null) {
+            return $this->model->allowedFilters();
+        }
+
+        return (new FilterSetAllowedFilterResolver($this->allowedFilters, $this->model))->resolve();
+    }
+
+    private function resolveFilterSet(FilterSet|string $filterSet): FilterSet
+    {
+        if ($filterSet instanceof FilterSet) {
+            return $filterSet;
+        }
+
+        $filterSets = $this->model->getFilterSets();
+
+        $filterSet = $filterSets->find($filterSet);
+
+        if ($filterSet === null) {
+            throw new \Exception('TODO'); // TODO
+        }
+
+        return $filterSet;
+    }
+
+    private function resolveFilterSetExtends(FilterSet $filterSet): AllowedFilterList
+    {
+        $allowedFilters = $filterSet->allowedFilters();
+
+        foreach ($filterSet->getExtends() as $extend) {
+            $allowedFilters = $allowedFilters->add(
+                ...$this->resolveFilterSetExtends(
+                    $this->resolveFilterSet($extend)
+                )->getAllowedFilters()
+            );
+        }
+
+        return $allowedFilters;
     }
 }
