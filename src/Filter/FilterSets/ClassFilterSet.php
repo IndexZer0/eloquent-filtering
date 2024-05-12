ec88cd0f8985c068b13b554c5e43afb343274dfa
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-05-13 00:35:46 +0100

    - Added ClassFilterSet class.

---
+++ src/Filter/FilterSets/ClassFilterSet.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace IndexZer0\EloquentFiltering\Filter\FilterSets;
+
+use IndexZer0\EloquentFiltering\Filter\Contracts\FilterSet as FilterSetContract;
+
+abstract class ClassFilterSet implements FilterSetContract
+{
+    protected array $extends = [];
+
+    public function name(): string
+    {
+        return static::class;
+    }
+
+    public function extends(string|array $extends): FilterSetContract
+    {
+        $this->extends = array_merge($this->extends, is_array($extends) ? $extends : [$extends]);
+        return $this;
+    }
+
+    public function getExtends(): array
+    {
+        return $this->extends;
+    }
+}
