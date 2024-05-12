27e1ff106dacb87d2b1f2443cf6785f517fa4f86
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-05-13 00:38:16 +0100

    - Added filter set methods to Filterable trait.

--- src/Filter/Traits/Filterable.php
+++ src/Filter/Traits/Filterable.php
@@ -9,7 +9,9 @@ use IndexZer0\EloquentFiltering\Filter\AllowedFilterResolver;
 use IndexZer0\EloquentFiltering\Filter\Contracts\FilterApplier;
 use IndexZer0\EloquentFiltering\Filter\Contracts\FilterParser;
 use IndexZer0\EloquentFiltering\Filter\Contracts\AllowedFilterList;
+use IndexZer0\EloquentFiltering\Filter\Contracts\FilterSet;
 use IndexZer0\EloquentFiltering\Filter\Filterable\Filter;
+use IndexZer0\EloquentFiltering\Filter\FilterSets\FilterSets;
 
 trait Filterable
 {
@@ -42,5 +44,17 @@ trait Filterable
         $defaultAllowedList = config('eloquent-filtering.default_allowed_filter_list', 'none');
 
         return $defaultAllowedList === 'none' ? Filter::none() : Filter::all();
+    }
+
+    public function filterSets(): FilterSets
+    {
+        return Filter::sets();
+    }
+
+    public function getFilterSets(): FilterSets
+    {
+        return $this->filterSets()->add(
+            Filter::set('default', $this->allowedFilters())
+        );
     }
 }
