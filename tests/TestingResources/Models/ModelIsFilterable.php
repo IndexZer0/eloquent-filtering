a43b21fba2bc3c9d7ac80bc923523d1996f89ae9
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-05-27 00:24:52 +0100

    - Added tests.

---
+++ tests/TestingResources/Models/ModelIsFilterable.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace IndexZer0\EloquentFiltering\Tests\TestingResources\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use IndexZer0\EloquentFiltering\Contracts\IsFilterable;
+use IndexZer0\EloquentFiltering\Filter\Traits\Filterable;
+
+class ModelIsFilterable extends Model implements IsFilterable
+{
+    use Filterable;
+
+    public function notFilterable(): BelongsTo
+    {
+        return $this->belongsTo(ModelIsNotFilterable::class);
+    }
+}
---
+++ tests/TestingResources/Models/ModelIsNotFilterable.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace IndexZer0\EloquentFiltering\Tests\TestingResources\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use IndexZer0\EloquentFiltering\Filter\Contracts\AllowedFilterList;
+use IndexZer0\EloquentFiltering\Filter\Filterable\Filter;
+use IndexZer0\EloquentFiltering\Filter\Traits\Filterable;
+
+class ModelIsNotFilterable extends Model
+{
+    use Filterable;
+
+    public function allowedFilters(): AllowedFilterList
+    {
+        return Filter::only(
+            Filter::field('name', ['$eq'])
+        );
+    }
+}
