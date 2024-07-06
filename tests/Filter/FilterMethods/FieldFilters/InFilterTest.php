3dc659ec91ec42558f90369a464ee3448adf8d41
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-07-06 17:20:52 +0100

    - Add null modifier tests for InFilter and NotInFilter.

--- tests/Filter/FilterMethods/FieldFilters/InFilterTest.php
+++ tests/Filter/FilterMethods/FieldFilters/InFilterTest.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use IndexZer0\EloquentFiltering\Filter\Filterable\Filter;
+use IndexZer0\EloquentFiltering\Filter\FilterType;
 use IndexZer0\EloquentFiltering\Tests\TestingResources\Models\Author;
 
 beforeEach(function (): void {
@@ -35,5 +36,38 @@ it('can perform $in filter', function (): void {
     $models = $query->get();
 
     expect($models->count())->toBe(2);
+
+});
+
+it('can perform $in filter with :null modifier', function (): void {
+
+    $query = Author::filter(
+        [
+            [
+                'target' => 'name',
+                'type'   => '$in:null',
+                'value'  => [
+                    'George Raymond Richard Martin',
+                    null,
+                ],
+            ],
+        ],
+        Filter::only(
+            Filter::field('name', [
+                FilterType::IN->withModifiers('null'),
+            ]),
+        )
+    );
+
+    $expectedSql = <<< SQL
+        select * from "authors" where ("name" in ('George Raymond Richard Martin') or "name" is null)
+        SQL;
+
+    expect($query->toRawSql())->toBe($expectedSql);
+
+    $models = $query->get();
+
+    expect($models->count())->toBe(1)
+        ->and($models->pluck('name')->toArray())->toBe(['George Raymond Richard Martin']);
 
 });
--- tests/Filter/FilterMethods/FieldFilters/NotInFilterTest.php
+++ tests/Filter/FilterMethods/FieldFilters/NotInFilterTest.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use IndexZer0\EloquentFiltering\Filter\Filterable\Filter;
+use IndexZer0\EloquentFiltering\Filter\FilterType;
 use IndexZer0\EloquentFiltering\Tests\TestingResources\Models\Author;
 
 beforeEach(function (): void {
@@ -35,5 +36,38 @@ it('can perform $notIn filter', function (): void {
     $models = $query->get();
 
     expect($models->count())->toBe(2);
+
+});
+
+it('can perform $notIn filter with :null modifier', function (): void {
+
+    $query = Author::filter(
+        [
+            [
+                'target' => 'name',
+                'type'   => '$notIn:null',
+                'value'  => [
+                    'George Raymond Richard Martin',
+                    null,
+                ],
+            ],
+        ],
+        Filter::only(
+            Filter::field('name', [
+                FilterType::NOT_IN->withModifiers('null'),
+            ]),
+        )
+    );
+
+    $expectedSql = <<< SQL
+        select * from "authors" where "name" not in ('George Raymond Richard Martin') and "name" is not null
+        SQL;
+
+    expect($query->toRawSql())->toBe($expectedSql);
+
+    $models = $query->get();
+
+    expect($models->count())->toBe(1)
+        ->and($models->pluck('name')->toArray())->toBe(['J. R. R. Tolkien']);
 
 });
