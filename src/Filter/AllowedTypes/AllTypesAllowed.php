e7a099a1e8b88f2969e500e547c68d338ec0e3d7
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-07-06 15:43:57 +0100

    - Update AllTypesAllowed concrete to use RequestedFilter.

--- src/Filter/AllowedTypes/AllTypesAllowed.php
+++ src/Filter/AllowedTypes/AllTypesAllowed.php
@@ -5,10 +5,11 @@ declare(strict_types=1);
 namespace IndexZer0\EloquentFiltering\Filter\AllowedTypes;
 
 use IndexZer0\EloquentFiltering\Filter\Contracts\AllowedTypes;
+use IndexZer0\EloquentFiltering\Filter\RequestedFilter;
 
 class AllTypesAllowed implements AllowedTypes
 {
-    public function contains(string $type): bool
+    public function contains(RequestedFilter $requestedFilter): bool
     {
         return true;
     }
