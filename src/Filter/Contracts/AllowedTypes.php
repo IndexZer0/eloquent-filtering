03cb839091d2e9d9468c98848d352af18dd51721
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-07-06 15:43:29 +0100

    - Change AllowedTypes interface to use RequestedFilter.

--- src/Filter/Contracts/AllowedTypes.php
+++ src/Filter/Contracts/AllowedTypes.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace IndexZer0\EloquentFiltering\Filter\Contracts;
 
+use IndexZer0\EloquentFiltering\Filter\RequestedFilter;
+
 interface AllowedTypes
 {
-    public function contains(string $type): bool;
+    public function contains(RequestedFilter $requestedFilter): bool;
 }
