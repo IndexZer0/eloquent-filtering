816d70602405aa3ce1ecd8053eafcbfec42cb255
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-07-07 17:27:03 +0100

    - Added RulesTest.

---
+++ tests/Filter/RulesTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+use IndexZer0\EloquentFiltering\Filter\Exceptions\MalformedFilterFormatException;
+use IndexZer0\EloquentFiltering\Filter\Filterable\Filter;
+use IndexZer0\EloquentFiltering\Filter\FilterType;
+use IndexZer0\EloquentFiltering\Tests\TestingResources\Models\IncludeRelationFields\Event;
+
+it('can have validation rules', function (): void {
+
+    Event::filter(
+        [
+            [
+                'target' => 'starting_at',
+                'type'   => '$between',
+                'value'  => [
+                    '2024-01-05', '2024-01-01',
+                ]
+            ],
+        ],
+        Filter::only(
+            Filter::field('starting_at', [FilterType::BETWEEN->withRules([
+                'value.0' => ['date', 'before:value.1'],
+                'value.1' => ['date', 'after:value.0']
+            ])]),
+        )
+    );
+
+    // TODO - MalformedFilterFormatException should extend laravel validation and also support supressible.
+
+})->throws(MalformedFilterFormatException::class, '"$between" filter does not match required format.');
