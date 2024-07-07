9bcf8edfe6a450da3b8bf6f0d2bf8f9fde278256
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-07-07 17:26:18 +0100

    - Add validateWith to PendingFilter.

--- src/Filter/Filterable/PendingFilter.php
+++ src/Filter/Filterable/PendingFilter.php
@@ -4,14 +4,19 @@ declare(strict_types=1);
 
 namespace IndexZer0\EloquentFiltering\Filter\Filterable;
 
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
 use IndexZer0\EloquentFiltering\Contracts\Target;
 use IndexZer0\EloquentFiltering\Filter\Context\FilterContext;
 use IndexZer0\EloquentFiltering\Filter\Contracts\AppliesToTarget;
+use IndexZer0\EloquentFiltering\Filter\Exceptions\MalformedFilterFormatException;
 use IndexZer0\EloquentFiltering\Filter\FilterCollection;
 use IndexZer0\EloquentFiltering\Filter\RequestedFilter;
 
 class PendingFilter
 {
+    protected array $validatedData = [];
+
     public function __construct(
         protected RequestedFilter $requestedFilter,
         protected string $filterFqcn,
@@ -62,13 +67,25 @@ class PendingFilter
         return sprintf($message, $target ? " for \"{$target}\"" : '');
     }
 
+    public function validateWith(array $rules): void
+    {
+        try {
+            $this->validatedData = Validator::validate(
+                $this->data,
+                array_merge_recursive($this->filterFqcn::format(), $rules),
+            );
+        } catch (ValidationException $ve) {
+            throw new MalformedFilterFormatException($this->requestedFilter->fullTypeString(), $ve);
+        }
+    }
+
     public function approveWith(
         ?Target           $target = null,
         ?FilterCollection $childFilters = null
     ): ApprovedFilter {
         return new ApprovedFilter(
             $this->filterFqcn,
-            $this->data,
+            $this->validatedData,
             $this->requestedFilter->modifiers,
             $target,
             $childFilters
