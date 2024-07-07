5f4f6b2f320999ada277a062436480dba3e252be
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-07-07 01:29:10 +0100

    - Added RequiredFilterException.

---
+++ src/Filter/Exceptions/RequiredFilterException.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace IndexZer0\EloquentFiltering\Filter\Exceptions;
+
+use Illuminate\Validation\ValidationException;
+use IndexZer0\EloquentFiltering\Filter\Contracts\AllowedFilter;
+use IndexZer0\EloquentFiltering\Filter\Contracts\FilterException;
+
+class RequiredFilterException extends ValidationException implements FilterException
+{
+    public static function fromRequiredFilters(AllowedFilter ...$allowedFilters): self
+    {
+        return self::withMessages(
+            collect($allowedFilters)->mapWithKeys(function(AllowedFilter $allowedFilter) {
+                return [$allowedFilter->getDescription() => $allowedFilter->getDescription() . ' is required.'];
+            })->toArray()
+        );
+    }
+}
