a746d4fad606b065d840664d3e3f0946b19bc70a
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-08-25 23:00:37 +0100

    - Update AllowedField to implement TargetedFilter and RequireableFilter.

--- src/Filter/AllowedFilters/AllowedField.php
+++ src/Filter/AllowedFilters/AllowedField.php
@@ -7,14 +7,14 @@ namespace IndexZer0\EloquentFiltering\Filter\AllowedFilters;
 use IndexZer0\EloquentFiltering\Contracts\Target;
 use IndexZer0\EloquentFiltering\Filter\AllowedTypes\AllowedType;
 use IndexZer0\EloquentFiltering\Filter\Context\FilterContext;
-use IndexZer0\EloquentFiltering\Filter\Contracts\AllowedFilter;
-use IndexZer0\EloquentFiltering\Filter\Contracts\AllowedFilterList;
+use IndexZer0\EloquentFiltering\Filter\Contracts\AllowedFilter\AllowedFilter;
+use IndexZer0\EloquentFiltering\Filter\Contracts\AllowedFilter\RequireableFilter;
+use IndexZer0\EloquentFiltering\Filter\Contracts\AllowedFilter\TargetedFilter;
 use IndexZer0\EloquentFiltering\Filter\Contracts\AllowedTypes;
-use IndexZer0\EloquentFiltering\Filter\Filterable\AllFiltersAllowed;
 use IndexZer0\EloquentFiltering\Filter\Filterable\PendingFilter;
-use IndexZer0\EloquentFiltering\Filter\Traits\CanBeRequired;
+use IndexZer0\EloquentFiltering\Filter\Traits\AllowedFilter\CanBeRequired;
 
-class AllowedField implements AllowedFilter
+class AllowedField implements AllowedFilter, TargetedFilter, RequireableFilter
 {
     use CanBeRequired;
 
@@ -29,11 +29,6 @@ class AllowedField implements AllowedFilter
      * Interface methods
      * -----------------------------
      */
-
-    public function allowedFilters(): AllowedFilterList
-    {
-        return new AllFiltersAllowed();
-    }
 
     public function getAllowedType(PendingFilter $pendingFilter): ?AllowedType
     {
@@ -50,7 +45,7 @@ class AllowedField implements AllowedFilter
 
     public function getTarget(PendingFilter $pendingFilter): Target
     {
-        return $this->target->getForApprovedFilter($pendingFilter);
+        return $this->target->getForFilterMethod($pendingFilter);
     }
 
     public function getDescription(): string
