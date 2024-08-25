c4ca6b52dec2f41713b7787e05877e5c4b8964d8
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-08-26 00:17:10 +0100

    - Use FilterType enum in test.

--- tests/Filter/FilterMethods/RelationFilters/DoesntHasFilterTest.php
+++ tests/Filter/FilterMethods/RelationFilters/DoesntHasFilterTest.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use IndexZer0\EloquentFiltering\Filter\Filterable\Filter;
+use IndexZer0\EloquentFiltering\Filter\FilterType;
 use IndexZer0\EloquentFiltering\Tests\TestingResources\Models\Author;
 
 beforeEach(function (): void {
@@ -19,7 +20,7 @@ it('can perform $doesntHas filter', function (): void {
             ],
         ],
         Filter::only(
-            Filter::relation('books', ['$doesntHas']),
+            Filter::relation('books', [FilterType::DOESNT_HAS]),
         )
     );
 
