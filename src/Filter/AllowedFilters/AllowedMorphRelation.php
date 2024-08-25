ad6b47868d5b2c4dbd598b380e705614c579c411
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-08-25 23:12:48 +0100

    - Add AllowedMorphRelation and AllowedMorphType.

---
+++ src/Filter/AllowedFilters/AllowedMorphRelation.php
@@ -0,0 +1,121 @@
+<?php
+
+declare(strict_types=1);
+
+namespace IndexZer0\EloquentFiltering\Filter\AllowedFilters;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Support\Collection;
+use IndexZer0\EloquentFiltering\Contracts\IsFilterable;
+use IndexZer0\EloquentFiltering\Contracts\Target;
+use IndexZer0\EloquentFiltering\Filter\AllowedTypes\AllowedType;
+use IndexZer0\EloquentFiltering\Filter\Context\FilterContext;
+use IndexZer0\EloquentFiltering\Filter\Contracts\AllowedFilter\AllowedFilter;
+use IndexZer0\EloquentFiltering\Filter\Contracts\AllowedFilter\DefinesAllowedChildFilters;
+use IndexZer0\EloquentFiltering\Filter\Contracts\AllowedFilter\RequireableFilter;
+use IndexZer0\EloquentFiltering\Filter\Contracts\AllowedFilter\TargetedFilter;
+use IndexZer0\EloquentFiltering\Filter\Contracts\AllowedFilterList;
+use IndexZer0\EloquentFiltering\Filter\Contracts\AllowedTypes;
+use IndexZer0\EloquentFiltering\Filter\Filterable\Filter;
+use IndexZer0\EloquentFiltering\Filter\Filterable\PendingFilter;
+use IndexZer0\EloquentFiltering\Filter\Traits\AllowedFilter\CanBeRequired;
+use IndexZer0\EloquentFiltering\Utilities\ClassUtils;
+
+class AllowedMorphRelation implements
+    AllowedFilter,
+    TargetedFilter,
+    RequireableFilter,
+    DefinesAllowedChildFilters
+{
+    use CanBeRequired;
+
+    protected Collection $includeRelationFields;
+
+    public function __construct(
+        protected Target                  $target,
+        protected AllowedTypes            $types,
+        protected AllowedFilterList        $allowedFilterList,
+    ) {
+        $this->includeRelationFields = collect();
+    }
+
+    /*
+     * -----------------------------
+     * Interface methods
+     * -----------------------------
+     */
+
+    public function allowedFilters(): AllowedFilterList
+    {
+        return $this->allowedFilterList;
+    }
+
+    public function getAllowedType(PendingFilter $pendingFilter): ?AllowedType
+    {
+        if (!$pendingFilter->is(FilterContext::MORPH_RELATION)) {
+            return null;
+        }
+
+        if (!$this->target->isFor($pendingFilter->desiredTarget())) {
+            return null;
+        }
+
+        return $this->types->get($pendingFilter->requestedFilter());
+    }
+
+    public function getTarget(PendingFilter $pendingFilter): Target
+    {
+        return $this->target;
+    }
+
+    public function getDescription(): string
+    {
+        return sprintf('"%s" filter', $this->target->target());
+    }
+
+    /*
+     * -----------------------------
+     * Specific methods
+     * -----------------------------
+     */
+
+    public function includeRelationFields(array $types): self
+    {
+        $this->includeRelationFields = collect($types);
+        return $this;
+    }
+
+    public function resolveAllowedFilters(string $modelFqcn): void
+    {
+        if ($this->includeRelationFields->isEmpty()) {
+            return;
+        }
+
+        foreach ($this->includeRelationFields as $relationType) {
+
+            $class = Relation::getMorphedModel($relationType);
+
+            $this->allowedFilters()->add(
+                new AllowedMorphType(
+                    $relationType,
+                    Filter::only(
+                        ...$this->getModelsAllowedFilters(new $class())->getAllowedFields()
+                    )
+                )
+            );
+        }
+
+        $this->includeRelationFields([]);
+    }
+
+    private function getModelsAllowedFilters(Model $model): ?AllowedFilterList
+    {
+        if (!ClassUtils::modelIsFilterable($model)) {
+            return null;
+        }
+
+        /** @var IsFilterable $model */
+        return $model->allowedFilters();
+    }
+}
---
+++ src/Filter/AllowedFilters/AllowedMorphType.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace IndexZer0\EloquentFiltering\Filter\AllowedFilters;
+
+use Exception;
+use IndexZer0\EloquentFiltering\Contracts\Target;
+use IndexZer0\EloquentFiltering\Filter\AllowedTypes\AllowedType;
+use IndexZer0\EloquentFiltering\Filter\Contracts\AllowedFilter\AllowedFilter;
+use IndexZer0\EloquentFiltering\Filter\Contracts\AllowedFilter\RequireableFilter;
+use IndexZer0\EloquentFiltering\Filter\Contracts\AllowedFilterList;
+use IndexZer0\EloquentFiltering\Filter\Filterable\PendingFilter;
+use IndexZer0\EloquentFiltering\Filter\Traits\AllowedFilter\CanBeRequired;
+
+class AllowedMorphType implements AllowedFilter, RequireableFilter
+{
+    use CanBeRequired;
+
+    public function __construct(
+        public string $type,
+        public AllowedFilterList $allowedFilterList
+    ) {
+    }
+
+    public function allowedFilters(): AllowedFilterList
+    {
+        return $this->allowedFilterList;
+    }
+
+    public function getAllowedType(PendingFilter $pendingFilter): ?AllowedType
+    {
+        throw new Exception('Not implemented');
+    }
+
+    public function getTarget(PendingFilter $pendingFilter): ?Target
+    {
+        return \IndexZer0\EloquentFiltering\Target\Target::alias($this->type);
+    }
+
+    public function getDescription(): string
+    {
+        throw new Exception('Not implemented');
+    }
+}
