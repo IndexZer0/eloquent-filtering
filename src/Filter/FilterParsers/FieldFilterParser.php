dc049ee5b5ee5176df70eaf207a0cf6ac82c6fe0
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-08-25 23:06:52 +0100

    - Update FieldFilterParser to check for PivotableFilter interface.

--- src/Filter/FilterParsers/FieldFilterParser.php
+++ src/Filter/FilterParsers/FieldFilterParser.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace IndexZer0\EloquentFiltering\Filter\FilterParsers;
 
-use IndexZer0\EloquentFiltering\Filter\AllowedFilters\AllowedField;
 use IndexZer0\EloquentFiltering\Filter\Builder\FilterBuilder;
 use IndexZer0\EloquentFiltering\Filter\Context\EloquentContext;
 use IndexZer0\EloquentFiltering\Filter\Contracts\AllowedFilter\AllowedFilter;
+use IndexZer0\EloquentFiltering\Filter\Contracts\AllowedFilter\PivotableFilter;
 use IndexZer0\EloquentFiltering\Filter\Contracts\AllowedFilter\TargetedFilter;
 use IndexZer0\EloquentFiltering\Filter\Contracts\AllowedFilterList;
 use IndexZer0\EloquentFiltering\Filter\Contracts\CustomFilterParser;
@@ -30,7 +30,7 @@ class FieldFilterParser implements CustomFilterParser
                 new EloquentContext(
                     $pendingFilter->model(),
                     $pendingFilter->relation(),
-                    $allowedFilter instanceof AllowedField && $allowedFilter->isPivot()
+                    is_a($allowedFilter, PivotableFilter::class) && $allowedFilter->isPivot()
                 )
             );
     }
