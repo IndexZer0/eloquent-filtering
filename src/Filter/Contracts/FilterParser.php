d87d413aebcefdaa69e91ee5c2ee224a04ee7109
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-08-25 22:31:42 +0100

    - Update FilterParser contract to take Model and Relation in order to be able to create EloquentContexts.

--- src/Filter/Contracts/FilterParser.php
+++ src/Filter/Contracts/FilterParser.php
@@ -4,9 +4,16 @@ declare(strict_types=1);
 
 namespace IndexZer0\EloquentFiltering\Filter\Contracts;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Relation;
 use IndexZer0\EloquentFiltering\Filter\FilterCollection;
 
 interface FilterParser
 {
-    public function parse(array $filters, AllowedFilterList $allowedFilterList): FilterCollection;
+    public function parse(
+        Model $model,
+        array $filters,
+        AllowedFilterList $allowedFilterList,
+        ?Relation $relation = null,
+    ): FilterCollection;
 }
