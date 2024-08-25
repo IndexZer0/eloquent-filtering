64e4b02ba44f42f98299bbb8ad8ac9b03a5dedd0
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-08-25 22:47:01 +0100

    - Rename RequiredFilterException::fromRequiredFilters() to ::fromAllowedFilters().

--- src/Filter/Exceptions/RequiredFilterException.php
+++ src/Filter/Exceptions/RequiredFilterException.php
@@ -5,15 +5,18 @@ declare(strict_types=1);
 namespace IndexZer0\EloquentFiltering\Filter\Exceptions;
 
 use Illuminate\Validation\ValidationException;
-use IndexZer0\EloquentFiltering\Filter\Contracts\AllowedFilter;
+use IndexZer0\EloquentFiltering\Filter\Contracts\AllowedFilter\AllowedFilter;
 use IndexZer0\EloquentFiltering\Filter\Contracts\FilterException;
 
 class RequiredFilterException extends ValidationException implements FilterException
 {
-    public static function fromRequiredFilters(AllowedFilter ...$allowedFilters): self
+    public static function fromAllowedFilters(AllowedFilter ...$allowedFilters): self
     {
         return self::withMessages(
-            collect($allowedFilters)->mapWithKeys(fn (AllowedFilter $allowedFilter) => [$allowedFilter->getDescription() => $allowedFilter->getDescription() . ' is required.'])->toArray()
+            collect($allowedFilters)
+                ->mapWithKeys(
+                    fn (AllowedFilter $allowedFilter) => [$allowedFilter->getDescription() => $allowedFilter->getDescription() . ' is required.']
+                )->toArray()
         );
     }
 }
