ef690b238147450b1700209ec1575f9d79561268
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-08-25 23:21:18 +0100

    - Update expected sql in tests to have columns qualified.

--- tests/Filter/PaginationTest.php
+++ tests/Filter/PaginationTest.php
@@ -34,13 +34,13 @@ it('works with pagination', function (): void {
 
     expect($queryLog->toArray())->toBe([
         [
-            "query"    => "select count(*) as aggregate from \"authors\" where \"name\" = ?",
+            "query"    => "select count(*) as aggregate from \"authors\" where \"authors\".\"name\" = ?",
             "bindings" => [
                 "George Raymond Richard Martin",
             ],
         ],
         [
-            "query"    => "select * from \"authors\" where \"name\" = ? limit 15 offset 0",
+            "query"    => "select * from \"authors\" where \"authors\".\"name\" = ? limit 15 offset 0",
             "bindings" => [
                 "George Raymond Richard Martin",
             ],
