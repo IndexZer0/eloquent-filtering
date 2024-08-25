2585412a0bb5a42b39d5cb79f0ea114127504dbe
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-08-26 00:33:33 +0100

    - Update KebabCaseFilter in test resources to use new way of defining filters.

--- tests/TestingResources/CustomFilters/KebabCaseFilter.php
+++ tests/TestingResources/CustomFilters/KebabCaseFilter.php
@@ -6,17 +6,18 @@ namespace IndexZer0\EloquentFiltering\Tests\TestingResources\CustomFilters;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\DB;
-use IndexZer0\EloquentFiltering\Filter\Filterable\ApprovedFilter;
-use IndexZer0\EloquentFiltering\Filter\FilterMethods\Abstract\AbstractFieldFilter;
+use IndexZer0\EloquentFiltering\Filter\Contracts\FilterMethod;
+use IndexZer0\EloquentFiltering\Filter\Contracts\FilterMethod\Targetable;
+use IndexZer0\EloquentFiltering\Filter\Traits\FilterMethod\FilterContext\FieldFilter;
 use IndexZer0\EloquentFiltering\Rules\TargetRules;
 
-class KebabCaseFilter extends AbstractFieldFilter
+class KebabCaseFilter implements FilterMethod, Targetable
 {
-    final public function __construct(
-        protected string $target,
+    use FieldFilter;
+
+    public function __construct(
         protected string $value,
     ) {
-
     }
 
     /*
@@ -38,32 +39,11 @@ class KebabCaseFilter extends AbstractFieldFilter
         ];
     }
 
-    public static function from(ApprovedFilter $approvedFilter): static
-    {
-        return new static(
-            $approvedFilter->target()->getReal(),
-            $approvedFilter->data_get('value'),
-        );
-    }
-
     public function apply(Builder $query): Builder
     {
-        return $query->where(DB::raw($this->target()), $this->value());
-    }
-
-    /*
-     * -----------------------------
-     * Filter specific methods
-     * -----------------------------
-     */
-
-    private function target(): string
-    {
-        return "LOWER(REPLACE({$this->target}, ' ', '-'))";
-    }
-
-    private function value(): string
-    {
-        return strtolower(str_replace(' ', '-', $this->value));
+        return $query->where(
+            DB::raw("LOWER(REPLACE({$this->eloquentContext->qualifyColumn($this->target)}, ' ', '-'))"),
+            strtolower(str_replace(' ', '-', $this->value)),
+        );
     }
 }
