f41381f6825b59dc5e34ed28cd588c704f9e6b5d
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-08-28 19:44:24 +0100

    - Update RequiredFilterTest and add new test scenario for scoped required fields.

--- tests/Filter/RequiredFilterTest.php
+++ tests/Filter/RequiredFilterTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use IndexZer0\EloquentFiltering\Filter\Exceptions\RequiredFilterException;
 use IndexZer0\EloquentFiltering\Filter\Filterable\Filter;
 use IndexZer0\EloquentFiltering\Filter\FilterType;
+use IndexZer0\EloquentFiltering\Target\Target;
 use IndexZer0\EloquentFiltering\Tests\TestingResources\CustomFilters\LatestFilter;
 use IndexZer0\EloquentFiltering\Tests\TestingResources\Models\Author;
 
@@ -21,8 +22,17 @@ it('throws RequiredFilterException when required filters have not been matched',
                     'books',
                     [FilterType::HAS],
                     Filter::only(
-                        Filter::field('title', [FilterType::LIKE])->required()
+                        Filter::field('title', [FilterType::LIKE])->required(),
                     )
+                )->required(),
+                Filter::morphRelation(
+                    'imageable',
+                    [FilterType::HAS_MORPH],
+                    Filter::morphType('articles',
+                        Filter::only(
+                            Filter::field('title', [FilterType::LIKE])->required()
+                        )
+                    )->required()
                 )->required(),
                 Filter::custom('$latest')->required()
             )
@@ -31,18 +41,73 @@ it('throws RequiredFilterException when required filters have not been matched',
         $this->fail('Should have thrown exception');
 
     } catch (RequiredFilterException $rfe) {
-        expect($rfe->getMessage())->toBe('"name" filter is required. (and 3 more errors)')
+        expect($rfe->getMessage())->toBe('"name" filter is required. (and 6 more errors)')
             ->and($rfe->errors())->toBe([
-                '"name" filter' => [
+                'Missing required filters.' => [
                     '"name" filter is required.',
-                ],
-                '"books" filter' => [
                     '"books" filter is required.',
+                    '"books" -> "title" filter is required.',
+                    '"imageable" filter is required.',
+                    '"imageable" -> "articles" filter is required.',
+                    '"imageable" -> "articles" -> "title" filter is required.',
+                    '"$latest" filter is required.',
                 ],
-                '"title" filter' => [
-                    '"title" filter is required.',
-                ],
-                '"$latest" filter' => [
+            ]);
+    }
+
+});
+
+it('only includes required errors when parent has been matched when using scoped', function (): void {
+
+    config()->set('eloquent-filtering.custom_filters', [LatestFilter::class]);
+
+    try {
+        Author::filter(
+            [
+                [
+                    'target' => 'books2',
+                    'type' => '$has',
+                ]
+            ],
+            Filter::only(
+                Filter::field('name', [FilterType::LIKE])->required(scoped: true),
+                Filter::relation(
+                    'books',
+                    [FilterType::HAS],
+                    Filter::only(
+                        Filter::field('title', [FilterType::LIKE])->required(scoped: true),
+                    )
+                )->required(scoped: true),
+                Filter::relation(
+                    Target::alias('books2', 'books'),
+                    [FilterType::HAS],
+                    Filter::only(
+                        Filter::field('title', [FilterType::LIKE])->required(scoped: true),
+                    )
+                )->required(scoped: true),
+                Filter::morphRelation(
+                    'imageable',
+                    [FilterType::HAS_MORPH],
+                    Filter::morphType('articles',
+                        Filter::only(
+                            Filter::field('title', [FilterType::LIKE])->required(scoped: true)
+                        )
+                    )->required(scoped: true)
+                )->required(scoped: true),
+                Filter::custom('$latest')->required(scoped: true)
+            )
+        );
+
+        $this->fail('Should have thrown exception');
+
+    } catch (RequiredFilterException $rfe) {
+        expect($rfe->getMessage())->toBe('"name" filter is required. (and 4 more errors)')
+            ->and($rfe->errors())->toBe([
+                'Missing required filters.' => [
+                    '"name" filter is required.',
+                    '"books" filter is required.',
+                    '"books2" -> "title" filter is required.',
+                    '"imageable" filter is required.',
                     '"$latest" filter is required.',
                 ],
             ]);
@@ -85,6 +150,11 @@ it('does not throw RequiredFilterException when required filters have been match
                     Filter::only(
                         Filter::field('title', [FilterType::LIKE])->required()
                     )
+                )->required(),
+                Filter::morphRelation(
+                    'imageable',
+                    [FilterType::HAS_MORPH],
+                    Filter::morphType('articles')->required()
                 )->required(),
                 Filter::custom('$latest')->required()
             )
