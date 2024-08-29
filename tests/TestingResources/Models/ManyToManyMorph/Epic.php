d8f3eb577becd73dc761404e4cce3ca77c168639
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-08-29 23:41:59 +0100

    - Added Many To Many Morph models for tests.

---
+++ tests/TestingResources/Models/ManyToManyMorph/Epic.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace IndexZer0\EloquentFiltering\Tests\TestingResources\Models\ManyToManyMorph;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
+use IndexZer0\EloquentFiltering\Contracts\IsFilterable;
+use IndexZer0\EloquentFiltering\Filter\Traits\Filterable;
+use IndexZer0\EloquentFiltering\Sort\Traits\Sortable;
+
+class Epic extends Model implements IsFilterable
+{
+    use Filterable;
+    use Sortable;
+
+    protected $guarded = [];
+
+    public function labels(): MorphToMany
+    {
+        return $this->morphToMany(Label::class, 'labelable');
+    }
+}
---
+++ tests/TestingResources/Models/ManyToManyMorph/Label.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace IndexZer0\EloquentFiltering\Tests\TestingResources\Models\ManyToManyMorph;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
+use IndexZer0\EloquentFiltering\Contracts\IsFilterable;
+use IndexZer0\EloquentFiltering\Filter\Traits\Filterable;
+use IndexZer0\EloquentFiltering\Sort\Traits\Sortable;
+
+class Label extends Model implements IsFilterable
+{
+    use Filterable;
+    use Sortable;
+
+    protected $guarded = [];
+
+    public function epics(): MorphToMany
+    {
+        return $this->morphedByMany(Epic::class, 'labelable');
+    }
+
+    public function issues(): MorphToMany
+    {
+        return $this->morphedByMany(Epic::class, 'labelable');
+    }
+}
