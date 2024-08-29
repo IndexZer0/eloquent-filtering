06e9a95f54db2698604d870382fa4e8006416687
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-08-29 22:13:07 +0100

    - Added scenarios to AllowedModifiersTest.

--- tests/Filter/AllowedModifiersTest.php
+++ tests/Filter/AllowedModifiersTest.php
@@ -6,6 +6,7 @@ use IndexZer0\EloquentFiltering\Filter\Exceptions\DeniedFilterException;
 use IndexZer0\EloquentFiltering\Filter\Exceptions\UnsupportedModifierException;
 use IndexZer0\EloquentFiltering\Filter\Filterable\Filter;
 use IndexZer0\EloquentFiltering\Filter\FilterType;
+use IndexZer0\EloquentFiltering\Filter\Types\Types;
 use IndexZer0\EloquentFiltering\Tests\TestingResources\Models\Author;
 
 beforeEach(function (): void {
@@ -39,7 +40,7 @@ it('defaults to have all supported modifiers allowed', function (): void {
 
 });
 
-it('can have only specific modifiers allowed', function (): void {
+it('can have only specific modifiers allowed | valid modifier', function (): void {
     $query = Author::filter(
         [
             [
@@ -65,6 +66,33 @@ it('can have only specific modifiers allowed', function (): void {
         ->and($models->pluck('name')->toArray())->toBe(['George Raymond Richard Martin']);
 
 });
+
+it('can have only specific modifiers allowed | invalid modifier', function (): void {
+    $query = Author::filter(
+        [
+            [
+                'target' => 'name',
+                'type'   => '$like:start',
+                'value'  => 'George Raymond Richard Martin',
+            ],
+        ],
+        Filter::only(
+            Filter::field('name', [FilterType::LIKE->withModifiers('end')]),
+        )
+    );
+
+    $expectedSql = <<< SQL
+        select * from "authors" where "authors"."name" LIKE '%George Raymond Richard Martin'
+        SQL;
+
+    expect($query->toRawSql())->toBe($expectedSql);
+
+    $models = $query->get();
+
+    expect($models->count())->toBe(1)
+        ->and($models->pluck('name')->toArray())->toBe(['George Raymond Richard Martin']);
+
+})->throws(DeniedFilterException::class, '"$like:start" filter for "name" is not allowed');;
 
 it('can have zero modifiers allowed', function (): void {
 
@@ -99,3 +127,20 @@ it('exceptions when unsupported modifier specified', function (): void {
     );
 
 })->throws(UnsupportedModifierException::class, '"unsupported-modifier" is not a supported modifier');
+
+it('exceptions when using AllTypesAllowed with invalid modifier', function (): void {
+
+    Author::filter(
+        [
+            [
+                'target' => 'name',
+                'type'   => '$like:this-modifier-does-not-exist',
+                'value'  => 'George Raymond Richard Martin',
+            ],
+        ],
+        Filter::only(
+            Filter::field('name', Types::all()),
+        )
+    );
+
+})->throws(DeniedFilterException::class, '"$like:this-modifier-does-not-exist" filter for "name" is not allowed');
