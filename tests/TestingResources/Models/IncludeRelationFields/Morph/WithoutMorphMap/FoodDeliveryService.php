0a6787980ae6affa58ea885c0696c3c0129ccab5
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-09-02 18:06:09 +0100

    - Add morph models for tests.

---
+++ tests/TestingResources/Models/IncludeRelationFields/Morph/WithoutMorphMap/FoodDeliveryService.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace IndexZer0\EloquentFiltering\Tests\TestingResources\Models\IncludeRelationFields\Morph\WithoutMorphMap;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+use IndexZer0\EloquentFiltering\Contracts\IsFilterable;
+use IndexZer0\EloquentFiltering\Filter\Contracts\AllowedFilterList;
+use IndexZer0\EloquentFiltering\Filter\Filterable\Filter;
+use IndexZer0\EloquentFiltering\Filter\FilterType;
+use IndexZer0\EloquentFiltering\Filter\Traits\Filterable;
+use IndexZer0\EloquentFiltering\Sort\Traits\Sortable;
+
+class FoodDeliveryService extends Model implements IsFilterable
+{
+    use Filterable;
+    use Sortable;
+
+    protected $guarded = [];
+
+    public function subscriptions(): MorphMany
+    {
+        return $this->morphMany(Subscription::class, 'subscribable');
+    }
+
+    public function allowedFilters(): AllowedFilterList
+    {
+        return Filter::only(
+            Filter::field('name', [FilterType::EQUAL]),
+        );
+    }
+}
