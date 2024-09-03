06f4510a456342d4414e53c42bea2b311e3b4e56
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-09-03 18:08:03 +0100

    - Remove un-needed RequiredFilterException::fromStrings() method.

--- src/Filter/Exceptions/RequiredFilterException.php
+++ src/Filter/Exceptions/RequiredFilterException.php
@@ -4,18 +4,9 @@ declare(strict_types=1);
 
 namespace IndexZer0\EloquentFiltering\Filter\Exceptions;
 
-use Illuminate\Support\Collection;
 use Illuminate\Validation\ValidationException;
 use IndexZer0\EloquentFiltering\Filter\Contracts\FilterException;
 
 class RequiredFilterException extends ValidationException implements FilterException
 {
-    public static function fromStrings(Collection $strings): self
-    {
-        return self::withMessages([
-            'Missing required filters.' => $strings->map(
-                fn ($string) => "{$string} filter is required."
-            )->toArray(),
-        ]);
-    }
 }
